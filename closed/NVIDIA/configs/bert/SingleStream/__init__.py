b7e9a5312b1b09ea0e6b7d356e8eb8c40028a4ad
DjoserKhemSimeu <djoser.simeu@gmail.com>
2025-05-12 11:23:28 +0200

    update for Quadro RTX 8000* Desktop

--- closed/NVIDIA/configs/bert/SingleStream/__init__.py
+++ closed/NVIDIA/configs/bert/SingleStream/__init__.py
@@ -1119,6 +1119,27 @@ class A5000x1(BenchmarkConfiguration):
     scenario = Scenario.SingleStream
     benchmark = Benchmark.BERT
 
+@ConfigRegistry.register(HarnessType.Custom, AccuracyTarget.k_99, PowerSetting.MaxP)
+class Quadro_RTX_8000x1(BenchmarkConfiguration):
+    system = System("Quadro RTX 8000", Architecture.Turing, 1)
+    bert_opt_seqlen = 270
+    coalesced_tensor = True
+    gpu_batch_size = 1
+    gpu_copy_streams = 1
+    gpu_inference_streams = 1
+    input_dtype = "int32"
+    input_format = "linear"
+    precision = "int8"
+    tensor_path = "${PREPROCESSED_DATA_DIR}/squad_tokenized/input_ids.npy,${PREPROCESSED_DATA_DIR}/squad_tokenized/segment_ids.npy,${PREPROCESSED_DATA_DIR}/squad_tokenized/input_mask.npy"
+    use_small_tile_gemm_plugin = False
+    single_stream_expected_latency_ns = 31000000
+    min_query_count=16
+    max_query_count=16
+    use_graphs = False
+    scenario = Scenario.SingleStream
+    benchmark = Benchmark.BERT
+
+
 
 @ConfigRegistry.register(HarnessType.Custom, AccuracyTarget.k_99, PowerSetting.MaxP)
 class AGX_Xavier(BenchmarkConfiguration):
